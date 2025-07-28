e16cb73f0189825250c85aaa9fe03da2fc59a557
TeoGale10 <teog1@umbc.edu>
2025-07-28 13:56:59 -0400

    upload sql files

--- project_sql/2_top_paying_job_skills.sql
+++ project_sql/2_top_paying_job_skills.sql
@@ -6,22 +6,23 @@ Question: What skills are required for the top-paying data analyst jobs?
     helping job seekers understand which skills to develop that align with top salaries
 */
 
+
 WITH top_paying_jobs AS (
-    SELECT	
+    SELECT
         job_id,
         job_title,
         salary_year_avg,
         name AS company_name
-    FROM
-        job_postings_fact
-    LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
-    WHERE
-        job_title_short = 'Data Analyst' AND 
-        job_location = 'Anywhere' AND 
-        salary_year_avg IS NOT NULL
-    ORDER BY
-        salary_year_avg DESC
-    LIMIT 10
+FROM
+    job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_title_short = 'Data Analyst' AND
+    job_location = 'Anywhere' AND
+    salary_year_avg IS NOT NULL
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10
 )
 
 SELECT 
@@ -31,7 +32,7 @@ FROM top_paying_jobs
 INNER JOIN skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
 ORDER BY
-    salary_year_avg DESC;
+    salary_year_avg DESC
 
 /*
 Here's the breakdown of the most demanded skills for data analysts in 2023, based on job postings:
